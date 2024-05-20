8fd7d52e342655ea2ec2af7b3b72afae1e363df5
clemencebc1 <143797406+clemencebc1@users.noreply.github.com>
2024-05-20 18:26:24 +0200

    Add files via upload

--- oracle.py
+++ oracle.py
@@ -1,8 +1,13 @@
 import networkx as nx 
 import matplotlib as plt
 import requetes 
-from matplotlib import animation
-from IPython.display import HTML
+from tkinter import * 
 
 G = requetes.json_vers_nx("data_100.txt")
  
+fenetre = Tk()
+
+label = Label(fenetre, text="Hello World")
+label.pack()
+
+fenetre.mainloop()
