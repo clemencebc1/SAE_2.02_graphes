a81b32407061c3a04e3bea0c7899aa7b15e2821e
clemencebc1 <143797406+clemencebc1@users.noreply.github.com>
2024-05-20 18:37:00 +0200

    Add files via upload

--- oracle.py
+++ oracle.py
@@ -7,7 +7,18 @@ G = requetes.json_vers_nx("data_100.txt")
  
 fenetre = Tk()
 
-label = Label(fenetre, text="Hello World")
+label = Label(fenetre, text="Oracle de Bacon")
 label.pack()
+
+value = StringVar()
+value.set("Kevin Bacon")
+acteur1 = Entry(fenetre, textvariable=value, width=20)
+acteur1.pack()
+
+acteur2 = Entry(fenetre, width=20)
+acteur2.pack()
+
+
+
 
 fenetre.mainloop()
